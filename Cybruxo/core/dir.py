c02fcda256102577293e3b69d39d35b1b8f5a2aa
𝐒𝐀𝐆𝐐𝐗 ®™ <176986778+Cybruxo@users.noreply.github.com>
2026-05-22 14:58:29 +0530

    Add files via upload

--- Cybruxo/core/dir.py
+++ Cybruxo/core/dir.py
@@ -1,1 +1,21 @@
+# Copyright (c) 2026 Cybruxo
+# Licensed under the MIT License.
+# This file is part of CYBRUXO
 
+
+import shutil
+from pathlib import Path
+
+from Cybruxo import logger
+
+
+def ensure_dirs():
+    """
+    Ensure that the necessary directories exist.
+    """
+    if not shutil.which("deno") or not shutil.which("ffmpeg"):
+        raise RuntimeError("Deno and FFmpeg must be installed and accessible in the system PATH.")
+
+    for dir in ["cache", "downloads"]:
+        Path(dir).mkdir(parents=True, exist_ok=True)
+    logger.info("Cache directories updated.")
